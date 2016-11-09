f1fb846eb871a210cae4091a104d0c0ab5e8a544
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2016-11-09 14:52:12 +0100

    update

--- personal/preload/wra-preload.el
+++ personal/preload/wra-preload.el
@@ -4,8 +4,11 @@
 
 ;;; Code
 
-; disable themeing through prelude
+; disable theming through prelude
 (setq prelude-theme nil)
+
+;; disable flyspell
+(setq prelude-flyspell nil)
 
 (provide 'wra-preload)
 ;;; wra-preload.el ends here
