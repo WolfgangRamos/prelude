e58cd68460779581a78d85d05c67797074b56e16
Wolfgang Ramos <wolfgang@ramosarhuis.de>
2017-11-06 21:04:43 +0100

    addition to last commit

--- personal/wra/wra-restclient.el
+++ personal/wra/wra-restclient.el
@@ -13,8 +13,8 @@
             (setq truncate-lines t)
             (whitespace-mode 0)))
 
-(add-hook 'restclient-mode-hook 
-(lambda (
-(yas-minor-mode))))
+(add-hook 'restclient-mode-hook
+          (lambda ()
+            (yas-minor-mode 1)))
 (provide 'wra-restclient)
 ;;; wra-restclient.el ends here
