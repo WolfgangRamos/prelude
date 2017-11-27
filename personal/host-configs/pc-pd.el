c3105e0bbfe1e16d466c649b08040c93c4991b46
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2017-11-27 14:28:03 +0100

    dir local config for editing fls entities

---
+++ personal/host-configs/pc-pd.el
@@ -0,0 +1,4 @@
+;; set directory local variables
+;;(dir-locals-set-class-variables 'fls-entity-description '((nxml-mode . ((nxml . "Entity\\|Property")))))
+
+;;(dir-locals-set-directory-class "z:/entities/" 'fls-entity-description) 
