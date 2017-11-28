c8a0253d5a4350ff30e41f5ae0b4cc87ad3fc2ec
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2017-11-28 11:36:08 +0100

    added projectile configuration

--- personal/host-configs/pc-pd.el
+++ personal/host-configs/pc-pd.el
@@ -1,4 +1,4 @@
 ;; set directory local variables
-;;(dir-locals-set-class-variables 'fls-entity-description '((nxml-mode . ((nxml . "Entity\\|Property")))))
+;;(dir-locals-set-class-variables 'fls-entity-description '((nxml-mode . ((nxml-section-element-name-regex . "Entity\\|Property") (nxml-heading-element-name-regexp . "")))))
 
 ;;(dir-locals-set-directory-class "z:/entities/" 'fls-entity-description) 
