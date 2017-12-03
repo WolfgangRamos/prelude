13580b8716420609c3a38fc8bfddbf9e30c756bc
Wolfgang Ramos <wolfgang@ramosarhuis.de>
2017-12-04 00:42:54 +0100

    associated xml with web-mode; first web-mode configuration

--- personal/wra/wra-xml.el
+++ personal/wra/wra-xml.el
@@ -8,15 +8,8 @@
 (require 'hideshow)
 (require 'sgml-mode)
 (require 'nxml-mode)
+(prelude-require-package 'web-mode)
 
-(add-to-list 'hs-special-modes-alist
-             '(nxml-mode
-               "<!--\\|<[^/>]*[^/]>"
-               "-->\\|</[^/>]*[^/]>"
-
-               "<!--"
-               sgml-skip-tag-forward
-               nil))
 ;; set indetation to 2 spaces
 (setq nxml-child-indent 2)
 (setq rng-nxml-auto-validate-flag nil)
@@ -33,8 +26,15 @@
               (flycheck-mode -1)
               )))
 
-;; optional key bindings, easier than hs defaults
-(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+;; use web-mode to edit xml files; it provides superior folding
+(add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
+
+(setq web-mode-enable-current-element-highlight t)
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 (provide 'wra-xml)
 ;;; wra-xml.el ends here
