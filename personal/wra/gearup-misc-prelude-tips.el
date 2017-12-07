87f3a430a1db1af36019f456853af76fc06a1c8a
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2017-12-07 06:48:07 +0100

    added ediff tips

--- personal/wra/gearup-misc-prelude-tips.el
+++ personal/wra/gearup-misc-prelude-tips.el
@@ -11,5 +11,19 @@
 (push "In vc-mode-line ':' is shown when a file has been modified." prelude-tips)
 (push "In vc-mode-line '!' is shown when a file contains conflicts." prelude-tips)
 
+;; emacs help
+(push "Hit <C-h r> to read emacs' manual in emacs." prelude-tips)
+
+;; ediff
+(push "Hit <n> in ediff to goto next difference." prelude-tips)
+(push "Hit <p> in ediff to goto previous difference." prelude-tips)
+(push "Hit <a> in ediff to copy current difference from A to B." prelude-tips)
+(push "Hit <!> in ediff to recompute differences." prelude-tips)
+(push "Hit <v> in ediff to scroll up." prelude-tips)
+(push "Hit <V> in ediff to scroll down." prelude-tips)
+
+;; emacsclient
+(push "Hit <C-x #> to close an emacs client frame." prelude-tips);; FIXME verify this
+
 (provide 'gearup-misc-prelude-tips)
 ;;; gearup-misc-prelude-tips.el ends here
