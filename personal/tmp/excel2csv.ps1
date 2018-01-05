d07b5e929d3cea0af1ab5e63d5b0c79b19eb0d3b
Wolfgang Ramos <wolfgang.ramos@fastleansmart.com>
2018-01-05 10:51:56 +0100

    added excel2csv script

---
+++ personal/tmp/excel2csv.ps1
@@ -0,0 +1,20 @@
+param (
+    [string]$infile = ""
+)
+
+Function ExcelToCSV ([string]$File)
+{ 
+    $excelFile = "$pwd\" + $File + ".xlsx"
+    $Excel = New-Object -ComObject Excel.Application
+    $Excel.Visible = $false
+    $Excel.DisplayAlerts = $false
+    $wb = $Excel.Workbooks.Open($excelFile)
+    foreach ($ws in $wb.Worksheets)
+    {
+        $ws.SaveAs("$pwd\" + $File + "_" + ($ws.name) + ".csv", 6)
+    }
+    $Excel.Quit()
+}
+
+ExcelToCSV -File "$infile"
+#(Get-Content ).replace(';', ',') | Set-Content c:\temp\test.txt
