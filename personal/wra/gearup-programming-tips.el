b2f3bfe43e12f1fcd3c1daf1d165869a6d8e2df5
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2018-02-23 05:37:15 +0100

    added gearup-programming-tips

---
+++ personal/wra/gearup-programming-tips.el
@@ -0,0 +1,13 @@
+(setq gearup-programming-tips nil)
+
+;; general
+(setq gearup-programming-tips 
+  (append gearup-programming-tips
+    '("Always do ONE thing at a time."
+      "Implement only what you need -- Follow YAGNI principle (You Ain't Gonna Need It).")))
+
+
+;; refactoring
+(setq gearup-programming-tips 
+  (append gearup-programming-tips
+    '("Break dependencies for testing by faking collaborator objects, eg by <Extract Interface>.")))
