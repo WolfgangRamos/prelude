a31c4137d2a9e2b4e33188d741a6073794305ff5
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2018-04-03 15:42:00 +0200

    added programming tips

--- personal/wra/gearup-programming-tips.el
+++ personal/wra/gearup-programming-tips.el
@@ -6,7 +6,11 @@
     '("Always do ONE thing at a time."
       "Implement only what you need -- Follow YAGNI principle (You Ain't Gonna Need It)."
       "Apply DEMETERS LAW where feasable: \"Only use fields and methods of direct neighbor objects.\""
-      "DEMETERS LAW improves separation of concerns at the implementation level.")))
+      "DEMETERS LAW improves separation of concerns at the implementation level."
+      "Make dependencies explicit using Constructor, Interface or Setter (DEPENDENCY) INJECTION."
+      "Keep GLUE CODE (the logic of constructing objects from their dependencies) out of BUSINESS LOGIC CODE classes."
+      "Don't use SINGLETON or BORG PATTERN to implement application scope objects. Use real objects and pass them around with DEPENDENCY INJECTION."
+      "If you seperate GLUE CODE from BUISNESS LOGIC, your busisness logic classes will be easier to instanciate in unit tests.")))
 
 
 ;; refactoring
@@ -18,7 +22,8 @@
       "Identify Responsibilities: Private methods can often be reduced by extracting them to another class."
       "FEATURE SKETCHES [LEGACY CODE] can help identify responsibilities and splitting up classes."
       "The INTERFACE SEGREGATION PRINCIPLE helps improving information hiding and separation of concerns at the interface level."
-      "The INTERFACE SEGREGATION PRINCIPLE can be a starting point for splitting up large classes.")))
+      "The INTERFACE SEGREGATION PRINCIPLE can be a starting point for splitting up large classes."
+      "Instance methods that don't use private members can be made static for testing.")))
       
       
       
