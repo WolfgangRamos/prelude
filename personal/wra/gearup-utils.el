55716bf04196dfbac5e8ca3049b7a8d67e938ba1
Wolfgang Ramos <wolfgang@ramosarhuis.de>
2018-04-22 00:02:11 +0200

    [util] toggle vertical/horizontal window split

--- personal/wra/gearup-utils.el
+++ personal/wra/gearup-utils.el
@@ -56,6 +56,35 @@ _f_ decrease   _d_ shrink               _s_ shrink
 
 (push "Hit <C-c s> to open gearup sizing menu." prelude-tips)
 
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+         (next-win-buffer (window-buffer (next-window)))
+         (this-win-edges (window-edges (selected-window)))
+         (next-win-edges (window-edges (next-window)))
+         (this-win-2nd (not (and (<= (car this-win-edges)
+                     (car next-win-edges))
+                     (<= (cadr this-win-edges)
+                     (cadr next-win-edges)))))
+         (splitter
+          (if (= (car this-win-edges)
+             (car (window-edges (next-window))))
+          'split-window-horizontally
+        'split-window-vertically)))
+    (delete-other-windows)
+    (let ((first-win (selected-window)))
+      (funcall splitter)
+      (if this-win-2nd (other-window 1))
+      (set-window-buffer (selected-window) this-win-buffer)
+      (set-window-buffer (next-window) next-win-buffer)
+      (select-window first-win)
+      (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "C-x |") 'toggle-window-split)
+
+(push "Hit <C-x |> to toggle between vertical and horizontal window split." prelude-tips)
+
 ;; reverse kill sexp
 (defun gearup-reverse-kill-sexp (&optional arg)
   "Kill sexp before point.
