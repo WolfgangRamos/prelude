3bcf482215c27572e16d4a7a55c73f879c5c83dd
Wolfgang Ramos <wolfgang.ramos@fastleansmart.com>
2018-07-09 09:33:03 +0200

    improved company configuration

--- personal/wra/wra-company.el
+++ personal/wra/wra-company.el
@@ -4,9 +4,21 @@
 
 ;;; Code
 (prelude-require-packages '(company))
+(prelude-require-package 'helm-company)
 (require 'company)
 
+
 (setq company-idle-delay 0.1)
+
+(defun gearup-company--set-company-keybindings ()
+  "Set keybindings for `company-mode'."
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-mode-map (kbd "M-#") 'company-complete))
+
+(gearup-company--set-company-keybindings)
 
 (provide 'wra-company)
 ;;; wra-company.el ends here
