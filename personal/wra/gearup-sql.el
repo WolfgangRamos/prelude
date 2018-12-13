7f6b40a91f5ed989c63ed681cc2cfcd92cb53d6a
Wolfgang Ramos <wolfgang.ramos@fastleansmart.com>
2018-12-13 16:43:06 +0100

    update

---
+++ personal/wra/gearup-sql.el
@@ -0,0 +1,10 @@
+;;; gearup-sql.el --- Prelude mode configuration
+
+;;; Commentary:
+
+;;; Code:
+
+(add-hook 'sql-mode-hook 'yas-minor-mode-on)
+
+(provide 'gearup-sql)
+;;; gearup-sql.el ends here
