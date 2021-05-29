64bd338fba0880df96916cae302e50e035738d69
Wolfgang Ramos <wolfgang@ramos.arhuis.de>
2021-05-29 11:24:52 +0200

    Fixed emacs start up script

--- personal/ms-windows/start-emacs-server-then-client.bat
+++ personal/ms-windows/start-emacs-server-then-client.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 
-IF EXIST "%HOME%\.emacs.d\server\server" DEL "%HOME%\emacs.d\server\server"
+IF EXIST %HOME%\.emacs.d\server\server DEL %HOME%\emacs.d\server\server
 
 SET PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;C:\msys64\mingw64\local\bin;%PATH%
 
-C:\msys64\mingw64\bin\runemacs.exe --daemon --server-file "%HOME%\.emacs.d\server\server"
-C:\msys64\mingw64\bin\emacsclientw.exe --no-wait --create-frame --server-file "%HOME%\.emacs.d\server\server" %1
+C:\msys64\mingw64\bin\emacsclientw.exe --no-wait --create-frame --alternate-editor "" %1
+
+
 
