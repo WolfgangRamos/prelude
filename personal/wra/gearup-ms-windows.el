c31359206fd352821d3beca257093eb83bc751a7
Wolfgang Ramos <wolfgang.ramos@fastleansmart.com>
2022-08-23 08:11:20 +0200

    Execute code in windows el library only on windows systems

--- personal/wra/gearup-ms-windows.el
+++ personal/wra/gearup-ms-windows.el
@@ -3,26 +3,28 @@
 ;;; Commentary:
 
 ;;; Code
-(setq w32-pass-lwindow-to-system t)
-(setq w32-pass-rwindow-to-system t)
-(setq w32-pass-apps-to-system t)
 
-(defun gearup-w32--recentf-save-wrapper (terminal)
-  "Save recentf list."
-  (recentf-save-list))
 
 (when (equal system-type 'windows-nt)
+  (setq w32-pass-lwindow-to-system t)
+  (setq w32-pass-rwindow-to-system t)
+  (setq w32-pass-apps-to-system t)
+  
+  (defun gearup-w32--recentf-save-wrapper (terminal)
+    "Save recentf list."
+    (recentf-save-list))
+
+  (prelude-require-packages '(powershell w32-browser))
+  (require 'w32-browser)
+
+  ;; setting GPG related variables
+  (custom-set-variables
+   '(epg-gpg-home-directory "~/.gnupg")
+   '(epg-gpg-program "c:/msys64/usr/bin/gpg.exe")
+   '(epg-gpgconf-program "c:/msys64/usr/bin/gpgconf.exe"))
+
   (add-hook 'delete-terminal-functions 'gearup-w32--recentf-save-wrapper) ; Windows doesn't know how to gracefully exit emacs daemon. So we save recentf list not on emacs exit, but on closing last client frame."
   (setenv "GIT_ASKPASS" "git-gui--askpass")) ; allow magit to do https authentication by GUI
-
-(prelude-require-packages '(powershell w32-browser))
-(require 'w32-browser)
-
-;; setting GPG related variables
-(custom-set-variables
- '(epg-gpg-home-directory "~/.gnupg")
- '(epg-gpg-program "c:/msys64/usr/bin/gpg.exe")
- '(epg-gpgconf-program "c:/msys64/usr/bin/gpgconf.exe"))
 
 (provide 'gearup-ms-windows)
 ;;; gearup-ms-windows.el ends here
